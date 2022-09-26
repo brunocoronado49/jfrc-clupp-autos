e9e8ccf39a830b8456f01bc0344f2913eeef269d
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-26 12:29:00 -0500

    Primer commit, agregando config de firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrfBxHvaYbt0lfnmQYZgwD0lJahWtLtao",
+  authDomain: "jfrc-clupp.firebaseapp.com",
+  projectId: "jfrc-clupp",
+  storageBucket: "jfrc-clupp.appspot.com",
+  messagingSenderId: "574501025042",
+  appId: "1:574501025042:web:071b344140cf30b392efcb",
+  measurementId: "G-WEE2W11F6M"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
